101c348e224e85deb5612cba5377bdf01a0d6830
DinizMaths <70189571+DinizMaths@users.noreply.github.com>
2020-09-04 21:14:03 -0300

    Update index.js

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import {
   StyledBody, 
   Screen, 
-  Keybord, 
-  KeybordButton, 
-  KeybordClearButton, 
-  KeybordZeroButton, 
-  KeybordEqualButton, 
-  KeybordDividesButton 
+  Keyboard, 
+  KeyboardButton, 
+  KeyboardClearButton, 
+  KeyboardZeroButton, 
+  KeyboardEqualButton, 
+  KeyboardDividesButton 
 } from './styles';
 
 function Calculator() {
@@ -19,6 +19,7 @@ function Calculator() {
     event.preventDefault();
   }
 
+  
   function handleClickClearButton() {
     setScreen('');
   }
@@ -26,7 +27,7 @@ function Calculator() {
   function handleGetValue(event) {
     setScreen(screen + event.target.value);
   }
-
+  
   function handleGetOperation(event) {
     setOperation(event.target.value);
     setScreen(screen + event.target.value);
@@ -35,12 +36,12 @@ function Calculator() {
   function calculateTheResult(str) {
     const number1 = Number(str.slice(0, str.indexOf(operation)));
     //slices the string from the start until it finds a mathematical operation
-
+    
     const number2 = Number(str.slice(str.indexOf(operation) + 1, str.length));
     //slices the string from the mathematical operation until the end
-
+    
     let result;
-
+    
     switch(operation) {
       case '+':
         result = number1 + number2;
@@ -58,43 +59,44 @@ function Calculator() {
         result = 'ERROR';
         break;
     }
-
+              
     return result.toString();
   }
-  
+            
   function handleClickEqualButton() {
     const result = calculateTheResult(screen);
     setScreen(result);
   }
-
+  
   return(
     <StyledBody>
       <form onSubmit={onSubmitForm}>
         <Screen>
           <h1>{screen}</h1>
         </Screen>
-        <Keybord>
-          <KeybordClearButton onClick={handleClickClearButton}>C</KeybordClearButton>
-          <KeybordEqualButton type="submit" onClick={handleClickEqualButton}>=</KeybordEqualButton>
+        
+        <Keyboard>
+          <KeyboardClearButton onClick={handleClickClearButton}>C</KeyboardClearButton>
+          <KeyboardEqualButton type="submit" onClick={handleClickEqualButton}>=</KeyboardEqualButton>
 
-          <KeybordButton value={7} onClick={handleGetValue}>7</KeybordButton>
-          <KeybordButton value={8} onClick={handleGetValue}>8</KeybordButton>
-          <KeybordButton value={9} onClick={handleGetValue}>9</KeybordButton>
-          <KeybordButton value={'+'} onClick={handleGetOperation}>&#x2b;</KeybordButton>
+          <KeyboardButton value={7} onClick={handleGetValue}>7</KeyboardButton>
+          <KeyboardButton value={8} onClick={handleGetValue}>8</KeyboardButton>
+          <KeyboardButton value={9} onClick={handleGetValue}>9</KeyboardButton>
+          <KeyboardButton value={'+'} onClick={handleGetOperation}>&#x2b;</KeyboardButton>
 
-          <KeybordButton value={4} onClick={handleGetValue}>4</KeybordButton>
-          <KeybordButton value={5} onClick={handleGetValue}>5</KeybordButton>
-          <KeybordButton value={6} onClick={handleGetValue}>6</KeybordButton>
-          <KeybordButton value={'-'} onClick={handleGetOperation}>&#x2212;</KeybordButton>
+          <KeyboardButton value={4} onClick={handleGetValue}>4</KeyboardButton>
+          <KeyboardButton value={5} onClick={handleGetValue}>5</KeyboardButton>
+          <KeyboardButton value={6} onClick={handleGetValue}>6</KeyboardButton>
+          <KeyboardButton value={'-'} onClick={handleGetOperation}>&#x2212;</KeyboardButton>
 
-          <KeybordButton value={1} onClick={handleGetValue}>1</KeybordButton>
-          <KeybordButton value={2} onClick={handleGetValue}>2</KeybordButton>
-          <KeybordButton value={3} onClick={handleGetValue}>3</KeybordButton>
-          <KeybordButton value={'*'} onClick={handleGetOperation}>&#xd7;</KeybordButton>
+          <KeyboardButton value={1} onClick={handleGetValue}>1</KeyboardButton>
+          <KeyboardButton value={2} onClick={handleGetValue}>2</KeyboardButton>
+          <KeyboardButton value={3} onClick={handleGetValue}>3</KeyboardButton>
+          <KeyboardButton value={'*'} onClick={handleGetOperation}>&#xd7;</KeyboardButton>
 
-          <KeybordZeroButton value={0} onClick={handleGetValue}>0</KeybordZeroButton>
-          <KeybordDividesButton value={'/'} onClick={handleGetOperation}>&#xf7;</KeybordDividesButton>
-        </Keybord>
+          <KeyboardZeroButton value={0} onClick={handleGetValue}>0</KeyboardZeroButton>
+          <KeyboardDividesButton value={'/'} onClick={handleGetOperation}>&#xf7;</KeyboardDividesButton>
+        </Keyboard>
       </form>
     </StyledBody>
   );
